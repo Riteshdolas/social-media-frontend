9b5666e4d651ae93ad577c1421ca722dbd44a95b
Ritesh dolas <dolasritesh97@gmail.com>
2025-06-30 12:42:20 +0530

    configurations

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
   ],
+  base: '/social-media-frontend/',
 })
