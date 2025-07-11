685baae6e4518833d4cb99d1eba0d2a3b1de1a80
Ritesh dolas <dolasritesh97@gmail.com>
2025-07-11 17:40:11 +0530

    wraped PostProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import UserContext from './context/UserContext.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import UserContext from "./context/UserContext.jsx";
+import { PostProvider } from "./context/PostContext.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-  <UserContext>
-    <App />
-  </UserContext>
+    <UserContext>
+      <PostProvider>
+        <App />
+      </PostProvider>
+    </UserContext>
   </BrowserRouter>
- 
-)
+);
