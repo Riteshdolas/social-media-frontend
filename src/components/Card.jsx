b51d3ba9286a8639b187de495433d3a0eb07e094
Ritesh dolas <dolasritesh97@gmail.com>
2025-07-25 15:45:35 +0530

    add like count logic and share feature

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,39 @@
 import React, { useState } from "react";
-import { FaRegHeart, FaRegComment, FaRegShareSquare } from "react-icons/fa";
+import {
+  FaRegHeart,
+  FaHeart,
+  FaRegComment,
+  FaRegShareSquare,
+} from "react-icons/fa";
 
 const PostCard = ({ username, avatar, time, content, image }) => {
-  const [like, setLike] = useState(0)
-  
+  const [like, setLike] = useState(0);
+  const [liked, setLiked] = useState(false);
+
+  const handleLikes = () => {
+    if (liked) {
+      setLike(like - 1);
+    } else {
+      setLike(like + 1);
+    }
+    setLiked(!liked);
+  };
+
+  function sharePost() {
+  if (navigator.share) {
+    navigator.share({
+      title: 'Check this post!',
+      text: 'I found this awesome post. Have a look:',
+      url: window.location.href  // current page link
+    })
+    .then(() => console.log('Shared successfully'))
+    .catch((error) => console.log('Error sharing:', error));
+  } else {
+    alert('Sharing not supported in your browser');
+  }
+}
+
+
   return (
     <div className="max-w-md mt-1 mx-auto bg-gray-800 shadow-lg rounded-2xl p-4 mb-6">
       {/* Header */}
@@ -31,20 +61,21 @@ const PostCard = ({ username, avatar, time, content, image }) => {
 
       {/* Actions */}
       <div className="flex items-end justify-around mt-4 text-gray-600 text-xl">
-        <button className="hover:text-red-500">
-            {like}
-          <FaRegHeart onClick={() =>{ 
-            setLike(like + 1)
-             
-            }}  />
+        <button className="hover:text-red-500 transition-colors duration-200">
+          {like}
+          {liked === true ? (
+            <FaHeart onClick={handleLikes} className="text-red-500 cursor-pointer transition-transform duration-200 hover:scale-110" />
+          ) : (
+            <FaRegHeart onClick={handleLikes} className="cursor-pointer transition-transform duration-200 hover:scale-110" />
+          )}
         </button>
         <button className="hover:text-blue-500">
           {12}
-          <FaRegComment onClick={() => console.log("add comment")}  />
+          <FaRegComment onClick={() => console.log("add comment")} />
         </button>
         <button className="hover:text-green-500">
           {2}
-          <FaRegShareSquare onClick={() => console.log("shared")} />
+          <FaRegShareSquare onClick={() => sharePost()} />
         </button>
       </div>
     </div>
